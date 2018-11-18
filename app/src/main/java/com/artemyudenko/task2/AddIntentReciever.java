ee848e5bd65d6624cfa4ffe316f8912da7251885
Artem Yudenko <s12676@pjwstk.edu.pl>
2018-11-18 14:50:48 +0100

    made noti work

--- app/src/main/java/com/artemyudenko/task2/AddIntentReciever.java
+++ app/src/main/java/com/artemyudenko/task2/AddIntentReciever.java
@@ -5,37 +5,34 @@ import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
-import android.util.Log;
-import android.widget.Toast;
 
 public class AddIntentReciever extends BroadcastReceiver {
 
+    public static final String CHANNEL_ID = "myChannelId";
+    public static final String CHANNEL_NAME = "myChannelName";
+
     @Override
     public void onReceive(Context context, Intent intent) {
+        String itemName = intent.getStringExtra("NAME");
+
         createNotificationChannel(context);
-        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context, "someChannel")
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context, CHANNEL_ID)
                     .setSmallIcon(R.mipmap.ic_launcher_round)
-                    .setContentTitle("My notification")
-                    .setContentText("Much longer text")
+                    .setContentTitle("New item is added!")
+                    .setContentText("Item " + itemName + " is added")
                     .setPriority(NotificationCompat.PRIORITY_DEFAULT);
             NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
             notificationManager.notify(0, mBuilder.build());
     }
 
     private void createNotificationChannel(Context context) {
-        // Create the NotificationChannel, but only on API 26+ because
-        // the NotificationChannel class is new and not in the support library
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel("someChannel", "name", importance);
-            channel.setDescription("despacito");
-            // Register the channel with the system; you can't change the importance
-            // or other notification behaviors after this
-            NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
-        }
+        int importance = NotificationManager.IMPORTANCE_DEFAULT;
+        NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, importance);
+        channel.setDescription("despacito");
+        NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
+        notificationManager.createNotificationChannel(channel);
     }
+
 }
--- app/src/main/java/com/artemyudenko/task2/MainActivity.java
+++ app/src/main/java/com/artemyudenko/task2/MainActivity.java
@@ -10,14 +10,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    AddIntentReciever addIntentReciever = new AddIntentReciever();
+
+    public static final String S_INTENT_FILTER = "sharedIntent";
+    public static final String CATEGORY = "sharedCat";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        IntentFilter intentFilter = new IntentFilter(S_INTENT_FILTER);
+        intentFilter.addCategory(CATEGORY);
+        registerReceiver(new AddIntentReciever(), intentFilter);
     }
-
-
-
 }
